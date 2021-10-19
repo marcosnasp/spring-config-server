adb63c59078182318f297b21333ff79270f24abd
Marcos Portela <marcosnasp@gmail.com>
2021-10-19 14:17:28 -0300

    Resolvendo problemas com a autenticação após atualização para spring-boot-2.5.5

--- src/test/java/com/example/spring/config/server/SpringConfigServerApplicationTests.java
+++ src/test/java/com/example/spring/config/server/SpringConfigServerApplicationTests.java
@@ -1,6 +1,6 @@
 package com.example.spring.config.server;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
